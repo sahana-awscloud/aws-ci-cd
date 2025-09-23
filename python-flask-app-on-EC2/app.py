c96d0fda30c7e9293806c0aefdae523f9c4b7cda
sahana-awscloud <sahanan3012@gmail.com>
2025-09-23 15:03:10 +0530

    Update app.py

--- python-flask-app-on-EC2/app.py
+++ python-flask-app-on-EC2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to python flask app hosted on AWS EC2 instance'
+    return 'Hello Sahana!!! Welcome to python flask app hosted on AWS EC2 instance'
 
 if __name__ == '__main__':
     app.run()
