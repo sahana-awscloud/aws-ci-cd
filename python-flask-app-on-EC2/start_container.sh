7bf11b82c216a1f63f7af3654e230b1a2020feee
sahana-awscloud <sahanan3012@gmail.com>
2025-09-23 16:17:32 +0530

    updated start_container.sh

--- python-flask-app-on-EC2/start_container.sh
+++ python-flask-app-on-EC2/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sahanaaws30/python-flask-app-on-ec2:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sahanaaws30/python-flask-app-on-ec2
